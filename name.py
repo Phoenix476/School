e8c2eaacc12b72072194e8bd5f17e514054d1650
phoenix <tohaukrainskih@gmail.com>
2015-09-26 18:18:44 +0700

    Сделано задание 1.2

--- name.py
+++ name.py
@@ -1,10 +1,18 @@
 import json
 
 
-def find_teacher(teacher):
-    teacher = teacher.split(' ')
-    for teacher_line in teachers:
-        a = 10
+def teacher_student(teacher, data_teachers, data_students):
+    students = []
+    for dict_el in data_teachers:
+        if teacher == '%s %s' % (dict_el['name'], dict_el['surname']):
+            classlist = dict_el['class']
+            for class_student in classlist:
+                for student in data_students:
+                    if student['class'] == class_student:
+                        students.append('%s %s' % (student['name'], student['surname']))
+    teacher_print = 'Учитель: %s \n' % teacher
+    return teacher_print, students
+
 
 def name(data):
     lst_name = []
@@ -35,19 +43,33 @@ teachers = open('Teachers.json', 'r')
 data_teachers = json.load(teachers)
 data_students = json.load(students)
 
+
 lst_name_teachers = name(data_teachers)
 lst_name_students = name(data_students)
-print(lst_name_students)
-print(lst_name_teachers)
+print('1.1.1:', '\n')
+for el in lst_name_students:
+    print(el)
+print('1.1.2:', '\n')
+for el in lst_name_teachers:
+    print(el)
 
 student_class = '5 А'
 lst_students_class = class_room(data_students, student_class)
-print(lst_students_class)
+print('1.1.3:', '\n')
+for el in lst_students_class:
+    print(el)
 
 schools = school(data_students)
-print(schools)
+print('1.1.4:', '\n')
+for el in schools:
+    print(el)
 
-teacher = 'Александр Сергеевич Чёрный'
+print('1.2:', '\n')
+teacher = 'Владимир Вышкин'
+teacher_print, students = teacher_student(teacher, data_teachers, data_students)
+print(teacher_print)
+for el in students:
+    print(el)
 
 
 
