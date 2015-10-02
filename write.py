f96d63ab1b96d6efc994eee8eee69cf3d56c1f30
phoenix <tohaukrainskih@gmail.com>
2015-10-02 19:00:45 +0700

    Доделать

--- write.py
+++ write.py
@@ -3,10 +3,11 @@ import json
 
 def student_add(data_students):
     # Записывает студента в файл
-    student = 'Иван Иванович Краснов'
-    student = student.split(' ')
+    student = 'Иван Иванович Краснов,Максим Максимович Максим,Дима Дмитриевич Дмитриев,Денис Денисович Денисов'
+    student = student.split(',')
+
     dict_student = {'name': student[0], 'surname': student[2], 'middle_name': student[1]}
-    dict_student = json.dumps(dict_student, ensure_ascii=False)
+    dict_student = json.dumps(dict_student, sort_keys=True, ensure_ascii=False, indent='')
     data_students.write(dict_student)
 
 
@@ -17,7 +18,7 @@ def teacher_add(data_teachers):
     class_teacher = '5А,11Б,7А'
     class_teacher = class_teacher.split(',')
     dict_teacher = {'name': teacher[0], 'surname': teacher[2], 'middle_name': teacher[1], 'class': class_teacher}
-    dict_teacher = json.dumps(dict_teacher, ensure_ascii=False)
+    dict_teacher = json.dumps(dict_teacher, sort_keys=True, ensure_ascii=False, indent='')
     data_teachers.write(dict_teacher)
 
 
@@ -31,11 +32,6 @@ data_students.close()
 data_teachers = open('My_Teachers', 'w')
 data_students = open('My_Students', 'w')
 
-z = 0
-l = 0
-while z < 5:
-    student_add(data_students)
-    z += 1
-while l < 3:
-    teacher_add(data_teachers)
-    l += 1
+student_add(data_students)
+teacher_add(data_teachers)
+
