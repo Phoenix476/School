2e261595f95752786a8c4b6742882bef90da6f54
phoenix <tohaukrainskih@gmail.com>
2015-10-02 18:48:21 +0700

    Исправил некоторые ошибки

--- write.py
+++ write.py
@@ -1,31 +1,41 @@
 import json
 
 
-def students_add(data_students):
-    # Записывает студентов в файл
-    list_students = []
-    while len(list_students) < 5:
-        student = 'Иван Иванович Краснов'
-        student = student.split(' ')
-        dict_student = {'name': student[0], 'surname': student[2], 'middle_name': student[1]}
-        list_students.append(dict_student)
-    list_students = json.dumps(list_students, ensure_ascii=False)
-    data_students.write(list_students)
+def student_add(data_students):
+    # Записывает студента в файл
+    student = 'Иван Иванович Краснов'
+    student = student.split(' ')
+    dict_student = {'name': student[0], 'surname': student[2], 'middle_name': student[1]}
+    dict_student = json.dumps(dict_student, ensure_ascii=False)
+    data_students.write(dict_student)
 
 
-def teachers_add(data_teachers):
-    # Записывает учителей в файл
-    list_teachers = []
-    while len(list_teachers) < 3:
-        teacher = 'Максим Максимович Меньшиков'
-        teacher = teacher.split(' ')
-        dict_teacher = {'name': teacher[0], 'surname': teacher[2], 'middle_name': teacher[1]}
-        list_teachers.append(dict_teacher)
-    list_teachers = json.dumps(list_teachers, ensure_ascii=False)
-    data_teachers.write(list_teachers)
+def teacher_add(data_teachers):
+    # Записывает учителя в файл
+    teacher = 'Максим Максимович Меньшиков'
+    teacher = teacher.split(' ')
+    class_teacher = '5А,11Б,7А'
+    class_teacher = class_teacher.split(',')
+    dict_teacher = {'name': teacher[0], 'surname': teacher[2], 'middle_name': teacher[1], 'class': class_teacher}
+    dict_teacher = json.dumps(dict_teacher, ensure_ascii=False)
+    data_teachers.write(dict_teacher)
+
+
+data_teachers = open('My_Teachers', 'r')
+data_students = open('My_Students', 'r')
+print(data_teachers.read())
+print(data_students.read())
+data_teachers.close()
+data_students.close()
 
 data_teachers = open('My_Teachers', 'w')
 data_students = open('My_Students', 'w')
 
-students_add(data_students)
-teachers_add(data_teachers)
+z = 0
+l = 0
+while z < 5:
+    student_add(data_students)
+    z += 1
+while l < 3:
+    teacher_add(data_teachers)
+    l += 1
